6305c282f7d19fc5c1d24144cb1f1b65ed86c3c4
MVP <mvpawar_b22@ce.vjti.ac.in>
2025-11-20 20:51:45 +0530

    Pushed SOTA Training files

---
+++ scripts/sota/train_all.sh
@@ -0,0 +1,5 @@
+python3 scripts/sota/train.py --model deeplabsv3+ --use_rgbnir
+python3 scripts/sota/train.py --model pspnet --use_rgbnir
+python3 scripts/sota/train.py --model lightsegnet --use_rgbnir
+python3 scripts/sota/train.py --model unet++ --use_rgbnir
+python3 scripts/sota/train.py --model unet3+ --base_ch 16 --use_rgbnir
